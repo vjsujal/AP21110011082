1aa98474eff96e433641fc27d3e92243a43aba63
Sujal VIjay <www.sujalvj121@gmail.com>
2024-04-22 13:59:54 +0530

    AXIOS

--- react-app/src/Components/ProductsSearch/productSearch.js
+++ react-app/src/Components/ProductsSearch/productSearch.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from "react";
+
 // import "../HomePage/homepage.css";
 import { Link } from "react-router-dom";
 
-const getProducts = async (productId) => {
-  const response = await fetch("http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=10&minPrice=1&maxPrice=1000000", {
-    method: "POST",
-    body: JSON.stringify({ id: productId }),
-    headers: {
-      Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzEyMTU5NzY5LCJpYXQiOjE3MTIxNTk0NjksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjQzZmIwOTk2LWRlZDktNDcyZS05MGMzLTI2MDAyMzFhYWUxYiIsInN1YiI6InZpamF5dmFyZ2l5YV9zQHNybWFwLmVkdS5pbiJ9LCJjb21wYW55TmFtZSI6ImdvU3VqYWwiLCJjbGllbnRJRCI6IjQzZmIwOTk2LWRlZDktNDcyZS05MGMzLTI2MDAyMzFhYWUxYiIsImNsaWVudFNlY3JldCI6Im1idmx6SXFJb09Bd3FMR2UiLCJvd25lck5hbWUiOiJTdWphbCBWaWpheXZhcmdpeWEiLCJvd25lckVtYWlsIjoidmlqYXl2YXJnaXlhX3NAc3JtYXAuZWR1LmluIiwicm9sbE5vIjoiQVAyMTExMDAxMTA4MiJ9.Vy4u6q2v8un33gNB1vXTNXwafmNzUo7mMG21acfzCgY",
-    },
-  });
-  const data = await response.json();
-  return data;
+import axios from 'axios';
+
+const getProducts = async () => {
+  try {
+    const response = await axios.post(
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzEyMTYwNTUzLCJpYXQiOjE3MTIxNjAyNTMsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjQzZmIwOTk2LWRlZDktNDcyZS05MGMzLTI2MDAyMzFhYWUxYiIsInN1YiI6InZpamF5dmFyZ2l5YV9zQHNybWFwLmVkdS5pbiJ9LCJjb21wYW55TmFtZSI6ImdvU3VqYWwiLCJjbGllbnRJRCI6IjQzZmIwOTk2LWRlZDktNDcyZS05MGMzLTI2MDAyMzFhYWUxYiIsImNsaWVudFNlY3JldCI6Im1idmx6SXFJb09Bd3FMR2UiLCJvd25lck5hbWUiOiJTdWphbCBWaWpheXZhcmdpeWEiLCJvd25lckVtYWlsIjoidmlqYXl2YXJnaXlhX3NAc3JtYXAuZWR1LmluIiwicm9sbE5vIjoiQVAyMTExMDAxMTA4MiJ9.eevBUsMmjKh1J1nNConA3Tqk6n0lfkx1Is7XmmxCd-s",
+        },
+        mode: 'cors'
+      }
+      
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error; // Rethrow the error to handle it at the calling site
+  }
 };
+
 
 
 
